c708b4422294d7f82133875991f84a68195cc8f7
David Ordovskiy <david.ordovsky@gmail.com>
2017-05-01 00:57:25 -0400

    fix spec

--- spec/pi_fraction_spec.rb
+++ spec/pi_fraction_spec.rb
@@ -3,16 +3,16 @@ require_relative './spec_helper.rb'
 describe 'pi_fraction' do
   it 'returns the closest approximation of pi to the given number' do
     fraction = pi_fraction(1)
-    expect(fraction).to eq("3 / 1 = 3.0")
+    expect(fraction).to eq("3.0 / 1.0 = 3.0")
   end
 
   it 'passes example test #1' do
     fraction = pi_fraction(0.01)
-    expect(fraction).to eq("22 / 7 = 3.142857142857143")
+    expect(fraction).to eq("22 / 7.0 = 3.142857142857143")
   end
 
   it 'passes example test #2' do
     fraction = pi_fraction(1/8)
-    expect(fraction).to eq("13 / 4 = 3.25")
+    expect(fraction).to eq("13 / 4.0 = 3.25")
   end
 end
