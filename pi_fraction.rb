93a7b1ab2e8a38130bfdde004173a0280899dfc2
David Ordovskiy <david.ordovsky@gmail.com>
2017-05-01 00:56:46 -0400

    add working solution

--- pi_fraction.rb
+++ pi_fraction.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def pi_fraction(gap)
-  # ... code here
+  pi = Math::PI
+
+  numerator = 3.0
+  denominator = 1.0
+
+  while (pi - numerator / denominator).abs > gap
+    denominator += 1.0
+    numerator = (denominator * pi).round
+  end
+
+  "#{numerator} / #{denominator} = #{numerator / denominator}"
 end
